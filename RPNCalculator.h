1f8b6a2e2e076d84fd8743a232d73c380dee1e39
EmilyMcCormack <72385642+EmilyMcCormack@users.noreply.github.com>
2024-08-08 17:49:07 +0100

    Added basic functions for calculator (add, subtract etc)

--- RPNCalculator.h
+++ RPNCalculator.h
@@ -19,18 +19,44 @@ public:
 	
 	//binary operators: 
 	void add() {
-
+		if (m_stackSize >= 2) {
+			T temp = m_stack[m_stackSize - 1];
+			temp += m_stack[m_stackSize - 2];
+			m_stack.pop_back();
+			m_stack -= 1;
+			m_stack[m_stackSize - 1] = temp; 
+		}
 	}
 
 	void subtract() {
+		if (m_stackSize >= 2) {
+			T temp = m_stack[m_stackSize - 2]; 
+			temp -= m_stack[m_stackSize - 1];
+			m_stack.pop_back();
+			m_stackSize -= 1; 
+			m_stack[m_stackSize - 1] = temp;
+		}
 
 	}
 
 	void multiply() {
-
+		if (m_stackSize >= 2) {
+			T temp = m_stack[m_stackSize - 1];
+			temp *= m_stack[m_stackSize - 2];
+			m_stack.pop_back();
+			m_stackSize -= 1; 
+			m_stack[m_stackSize - 1] = temp; 
+		}
 	}
 
 	void divide() {
+		if (m_stackSize >= 2) {
+			T temp = m_stack[m_stackSize - 2];
+			temp /= m_stack[m_stackSize - 1];
+			m_stack.pop_back();
+			m_stackSize -= 1;
+			m_stack[m_stackSize - 1] = temp;
+		}
 
 	}
 };
