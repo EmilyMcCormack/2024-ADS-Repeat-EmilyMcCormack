29429c687f8a4e32edd45bdd6f9466b0510c8212
EmilyMcCormack <72385642+EmilyMcCormack@users.noreply.github.com>
2024-08-18 21:00:29 +0100

    Updated menu + Allowed for doubles
    Updated menu functionality and allowed calculator to calculate doubles

--- RPNCalculator.cpp
+++ RPNCalculator.cpp
@@ -6,58 +6,87 @@
 
 using namespace std;
 
-int main() 
+int main()
 {
 	cout << "Enter c to clear the stack \n";
 	cout << "s to square\nn to negate\n p to pop current value\nq to quit\n\n";
-	
-	RPNCalculator<double> calc{};
-	bool running = true; 
-	string buff{};
-	
 
-	string buff;
-	cout << "X>";
-	while (getline(cin, buff)) {
-		if (buff.size() == 1) {
-			switch (buff[0]) {
-			case '+':
-				calculator.add();
-				break;
-			case '-':
-				calculator.subtract();
-				break;
-			case '*':
-				calculator.multiply();
-				break;
-			case '/':
-				calculator.divide();
-				break;
-			case 'square':
-				calculator.square();
-				break;
-			case 'negate':
-				calculator.negate();
-				break;
-			case 'pop':
-				calculator.pop();
-				break;
-			case 'clear':
-				calculator.clear();
-				break;
-			case 'return 0':
-				return 0;
-			default:
-				auto val = stod(buff);
-				calculator.push(val);
+	RPNCalculator<double> calc{};
+	bool running = true;
+	string buff{};
+	while (running)
+	{
+		if (calc.isEmpty())
+		{
+			cout << "X> ";
+		}
+		else
+		{
+			cout << calc.value() << "> ";
+		}
+		getline(cin, buff);
+		// Check the first characters only 
+		switch (buff[0])
+		{
+		case '/': {
+			calc.divide();
+			break;
+		}
+		case '*': {
+			calc.multiply();
+			break;
+		}
+		case 'c': {
+			calc.clear();
+			break;
+		}
+		case 's': {
+			calc.square();
+			break;
+		}
+		case 'n': {
+			calc.negate();
+			break;
+		}
+		case 'p': {
+			calc.pop();
+			break;
+		}
+		case 'q': {
+			running = false;
+			break;
+		}
+		case '+':
+			[[fallthrough]];
+		case '-':
+			// Allow for doubles to be input
+			if (buff.size() == 1 || buff[1] == ' ' || buff[1] == '\n')
+			{
+				if (buff[0] == '-')
+				{
+					calc.subtract();
+				}
+				else
+				{
+					calc.add();
+				}
 				break;
 			}
+
+		default:
+		{
+			try
+			{
+				auto val = stod(buff);
+				calc.push(val);
+			}
+			catch (exception)
+			{
+				cout << "invalid input" << endl;
+			}
+			break;
 		}
-		else {
-			auto val = stod(buff);
-			calculator.push(val);
 		}
-		cout << (calculator.isEmpty()) ? "X" : calculator.value() << "> ";
 	}
 	return 0; 
 }
