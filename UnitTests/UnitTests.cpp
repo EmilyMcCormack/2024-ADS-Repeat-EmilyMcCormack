cccb8b28d8982d8081d78947986d2838c45f46a7
EmilyMcCormack <72385642+EmilyMcCormack@users.noreply.github.com>
2024-08-19 11:12:33 +0100

    Added unit tests but working on error
    I have an error with Assert::Equals in my unit tests and I cannot figure it out. Will try to get it done before deadline

--- UnitTests/UnitTests.cpp
+++ UnitTests/UnitTests.cpp
@@ -1,5 +1,7 @@
 #include "pch.h"
 #include "CppUnitTest.h"
+#include "../2024-ADS-Repeat-EmilyMcCormack/RPNCalculator.h"
+#include <random> 
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
@@ -9,8 +11,73 @@ namespace UnitTests
 	{
 	public:
 		
-		TEST_METHOD(TestMethod1)
+		TEST_METHOD(TestAdd)
 		{
+			RPNCalculator<int> calc{};
+			std::mt19937 rng{ std::random_device()() };
+			// check 1000 random calculations
+			for (auto i = 0; i < 1000; i++) {
+				calc.clear();
+				auto a = rng();
+				auto b = rng();
+				auto result = a + b;
+				calc.push(a);
+				calc.push(b);
+				calc.add();
+				Assert::AreEqual(result, calc.value());
+			}
+		}
+
+		TEST_METHOD(TestSubtract)
+		{
+
+			RPNCalculator<int> calc{};
+			std::mt19937 rng{ std::random_device()() };
+			// check 1000 random calculations
+			for (auto i = 0; i < 1000; i++) {
+				calc.clear();
+				auto a = rng();
+				auto b = rng();
+				auto result = a - b;
+				calc.push(a);
+				calc.push(b);
+				calc.subtract();
+				Assert::AreEqual(result, calc.value());
+			}
+		}
+
+		TEST_METHOD(TestMultiply)
+		{
+			RPNCalculator<int> calc{};
+			std::mt19937 rng{ std::random_device()() };
+			// check 1000 random calculations
+			for (auto i = 0; i < 1000; i++) {
+				calc.clear();
+				auto a = rng();
+				auto b = rng();
+				auto result = a * b;
+				calc.push(a);
+				calc.push(b);
+				calc.multiply();
+				Assert::AreEqual(result, calc.value());
+			}
+		}
+
+		TEST_METHOD(TestDivide)
+		{
+			RPNCalculator<int> calc{};
+			std::mt19937 rng{ std::random_device()() };
+			// check 1000 random calculations
+			for (auto i = 0; i < 1000; i++) {
+				calc.clear();
+				auto a = rng();
+				auto b = rng();
+				auto result = a / b;
+				calc.push(a);
+				calc.push(b);
+				calc.divide();
+				Assert::AreEqual(result, calc.value());
+			}
 		}
 	};
 }
